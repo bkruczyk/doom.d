1f933301a7c8ee2973b1d8a93b4cab8fbeb6ac8a
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2022-04-25 09:10:23 +0200

    paredit tweaks

--- modules/editor/paredit/config.el
+++ modules/editor/paredit/config.el
@@ -9,5 +9,5 @@
     (map! :map paredit-mode-map :i "C-j" #'paredit-newline)
     (map! :map paredit-mode-map :i "C-k" #'paredit-kill)
     (map! :map paredit-mode-map :n "C"   #'+paredit/evil-change-line)
-    (map! "M-l" #'paredit-forward-slurp-sexp)
-    (map! "M-h" #'paredit-forward-barf-sexp)))
+    (map! :map paredit-mode-map "M-l" #'paredit-forward-slurp-sexp)
+    (map! :map paredit-mode-map "M-h" #'paredit-forward-barf-sexp)))
