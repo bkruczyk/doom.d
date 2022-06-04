da4329df6b18827a995b5b6cdf04997c79fcd8e2
Bartłomiej Kruczyk <bartlomiej.kruczyk@gmail.com>
2022-06-04 21:34:53 +0200

    Add frame maximizing hotkey

--- config.el
+++ config.el
@@ -2,22 +2,34 @@
 
 ;;; Notes
 
-;; M-m   back-to-indentation!
-;; _     does the same in Vim normal mode
+;; Emacs
+;; M-m     | back-to-indentation!
+;; C-x +   | balance windows
+;; C-x C-o | delete blank lines
+;;
+;; bury-buffer
+;; list-matching-lines
+;; goal-column
+;;
+;; Vim
+;; _      | does the same in Vim normal mode
+;; g;     | (Vim) jump to the place of last edit
 
 ;;; doom
-(setq doom-font (font-spec :family "Roboto Mono" :size 14))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 12))
 (setq doom-theme 'doom-solarized-dark-high-contrast)
 (setq doom-themes-enable-bold nil)
 (setq doom-themes-enable-italic nil)
 
-;;; decent settings for non doom themes like modus-operandi and modus vivendi
+(map! :nvi "s-<return>" #'toggle-frame-maximized)
+
+;;; decent settings for non-doom themes like modus-operandi and modus-vivendi
 (doom-themes-set-faces nil
   '(fringe :background nil)
   '(line-number :background nil)
   '(line-number-current-line :inherit 'hl-line :background nil))
 
-;;; org-mode
+;; org-mode
 (setq org-directory "~/org/")
 (setq org-log-done 'time)
 
